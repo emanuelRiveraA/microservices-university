c1a71f649d7faa2b170d90196105fb211af27f2a
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2023-10-31 17:10:41 -0600

    Se agregan los metodos para realizar las consultas

--- school-service/src/main/java/com/hexagon/schoolservice/SchoolService.java
+++ school-service/src/main/java/com/hexagon/schoolservice/SchoolService.java
@@ -1,5 +1,29 @@
 package com.hexagon.schoolservice;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hexagon.schoolservice.entity.School;
+import com.hexagon.schoolservice.repository.SchoolRepository;
+
+@Service
 public class SchoolService {
 
+	@Autowired
+	private SchoolRepository schoolRepository;
+	
+	public School addSchool(School school) {
+		return schoolRepository.save(school);
+	}
+	
+	public List<School> fetchSchools() {
+		return schoolRepository.findAll();
+	}
+	
+	public Optional<School> fetchSchoolById(int id) {
+		return schoolRepository.findById(id);
+	}
 }
