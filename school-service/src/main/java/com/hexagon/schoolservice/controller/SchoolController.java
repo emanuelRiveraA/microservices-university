bf7bf5127ac930542dd70085d701dfde1e0e544a
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2023-10-31 17:17:54 -0600

    Se agregan anotaciones

--- school-service/src/main/java/com/hexagon/schoolservice/controller/SchoolController.java
+++ school-service/src/main/java/com/hexagon/schoolservice/controller/SchoolController.java
@@ -4,15 +4,19 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hexagon.schoolservice.SchoolService;
 import com.hexagon.schoolservice.entity.School;
 
+@CrossOrigin("*")
+@RequestMapping(value = "/school")
 @RestController
 public class SchoolController {
 	
