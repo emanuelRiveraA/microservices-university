b818aef796e18a9f798cd54541e8c9f8a16e8c46
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2023-11-01 13:09:19 -0600

    Se crea metodo para crear student

--- student-service/src/main/java/com/hexagon/studentservice/controller/StudentController.java
+++ student-service/src/main/java/com/hexagon/studentservice/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.hexagon.studentservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hexagon.studentservice.entity.Student;
 import com.hexagon.studentservice.service.StudentService;
 
 @CrossOrigin("*")
@@ -14,6 +18,11 @@ public class StudentController {
 	
 	@Autowired
 	private StudentService studentService; 
+	
+	@PostMapping
+	public ResponseEntity<?> createStudent(@RequestBody Student student){
+		return studentService.createStudent(student);
+	}
 	
 
 }
