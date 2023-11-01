741d3ac08f824c7a7848c2e0f80dd6e006a99f3c
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2023-11-01 12:01:41 -0600

    Se crea metodo para crear un Student

--- student-service/src/main/java/com/hexagon/studentservice/service/StudentService.java
+++ student-service/src/main/java/com/hexagon/studentservice/service/StudentService.java
@@ -1,5 +1,25 @@
 package com.hexagon.studentservice.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.hexagon.studentservice.entity.Student;
+import com.hexagon.studentservice.repository.StudentRepository;
+
+@Service
 public class StudentService {
 
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	public ResponseEntity<?> createStudent(Student student){
+		try {			
+			return new ResponseEntity<Student>(studentRepository.save(student), HttpStatus.OK);
+		}catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}	
+	}
+	
 }
