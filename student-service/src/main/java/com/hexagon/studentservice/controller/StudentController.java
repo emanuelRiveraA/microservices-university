08cec4e057ff62531747b5b91ad2a92847a1430c
Emanuel Rivera <riveraaemanuel8418@gmail.com>
2023-11-01 13:12:11 -0600

    Se crean metodos para consultar students

--- student-service/src/main/java/com/hexagon/studentservice/controller/StudentController.java
+++ student-service/src/main/java/com/hexagon/studentservice/controller/StudentController.java
@@ -3,6 +3,8 @@ package com.hexagon.studentservice.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,5 +26,14 @@ public class StudentController {
 		return studentService.createStudent(student);
 	}
 	
+	@GetMapping
+	public ResponseEntity<?> fetchStudents(){
+		return studentService.fetchStudents();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> fetchStudentById(@PathVariable String id){
+		return studentService.fetchStudentById(id);
+	}
 
 }
